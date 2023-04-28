7d8a996552baa620692121a33fe34c4b8fc8ea47
proffitlemon@gmail.com <45953538+Henryn21@users.noreply.github.com>
2023-04-28 16:59:22 -0700

    Added icons and fixed styling on search page
    Search page started, icons added, some styling added

--- upcycling-recipe-book/src/components/SearchBar.js
+++ upcycling-recipe-book/src/components/SearchBar.js
@@ -1,10 +1,15 @@
 import logoImage from "../images/UpLogo.PNG";
 import "../stylesheets/SearchBar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 function SearchBar() {
     return (
         <nav className="searchHeader">
             <img src={logoImage} className="logo"/>
-            <input type="search" placeholder="Search For Recipes" className="searchInput"/>
+            <div className="searchDiv">
+                <label htmlFor="searchInput" className="searchIcon"><FontAwesomeIcon icon={faMagnifyingGlass} /></label>
+                <input type="search" placeholder="Search For Recipes" className="searchInput" id="searchInput"/>
+            </div>
         </nav>
     )
 }
--- upcycling-recipe-book/src/components/SearchPage.js
+++ upcycling-recipe-book/src/components/SearchPage.js
@@ -4,8 +4,15 @@ function SearchPage() {
     return (
         <div>
             <SearchBar/>
-            <div>
-                Hello
+            <div className="searchPageBody">
+                <div className="searchPageHero">
+                </div>
+                <div className="searchCategories">
+                    <div className="category"></div>
+                </div>
+                <div className="searchRecommendations">
+                    <div className="product"></div>
+                </div>
             </div>
         </div>
     )
