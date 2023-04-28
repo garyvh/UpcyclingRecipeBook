85664b9fe87abd5a99304fd39e08e52716a2a5bb
proffitlemon@gmail.com <45953538+Henryn21@users.noreply.github.com>
2023-04-27 18:02:50 -0700

    Started header
    Added header component (nav with h1s, image),
    started styling: flex with gap, maybe change to centered

--- upcycling-recipe-book/src/components/TheHeader.js
+++ upcycling-recipe-book/src/components/TheHeader.js
@@ -1,8 +1,14 @@
-
+import logoImage from "../images/UpLogo.PNG";
+import "./TheHeader.css";
 function TheHeader() {
     return (
-        <nav>
-            Hello
+        <nav className="myNav">
+            <img src={logoImage}/>
+            <h1>Home</h1>
+            <h1>Recipes</h1>
+            <h1>Sign Up</h1>
+            <h1>Log In</h1>
+            <h1>Help</h1>
         </nav>
     )
 }
