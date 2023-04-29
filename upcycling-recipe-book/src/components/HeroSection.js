2906e1dc6c1488c3c71b8afa11ed9ffc8ed15a8c
Gary Hoang <garyhoangvinh@gmail.com>
2023-04-28 17:10:03 -0700

    React project: added many more stylings to front page, not complete yet needs images and adjustments

--- upcycling-recipe-book/src/components/HeroSection.js
+++ upcycling-recipe-book/src/components/HeroSection.js
@@ -2,8 +2,8 @@ import '../stylesheets/HeroSection.css'
 
 function HeroSection() {
     return (
-        <div>
-            <h2>Upcycle for a Better Future</h2>
+        <div id="hero-section">
+            <h2>Upcycle for a Better Future:</h2>
             <h1>Recycle Old Materials into New</h1>
         </div>
     )
--- upcycling-recipe-book/src/components/Homepage.js
+++ upcycling-recipe-book/src/components/Homepage.js
@@ -1,13 +1,18 @@
+import '../stylesheets/Homepage.css'
 import TheHeader from "./TheHeader";
 import Gallery from "./Gallery";
 import HeroSection from "./HeroSection";
 
 function Homepage() {
     return (
-        <div>
+        <div id="homepage">
+            <div id="sideBarContainer">
+                <span id="sideBarCircle"></span>
+            </div>
             <TheHeader></TheHeader>
             <HeroSection></HeroSection>
             <Gallery></Gallery>
+            <footer></footer>
         </div>
     )
 }
