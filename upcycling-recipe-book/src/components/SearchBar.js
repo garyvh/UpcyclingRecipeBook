8a121732d7f8107a36b743e4fcc74ff3a682ec42
Gary Hoang <garyhoangvinh@gmail.com>
2023-05-05 13:54:47 -0700

    async fetch works fine react

--- upcycling-recipe-book/src/components/SearchBar.js
+++ upcycling-recipe-book/src/components/SearchBar.js
@@ -5,7 +5,7 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
 function SearchBar() {
-    const BACKEND_PATH = "/";
+    const BACKEND_PATH = "http://localhost:3500/recipeList/retrieve/";
 
     const [textValue, setTextValue] = useState("");
 
@@ -15,11 +15,18 @@ function SearchBar() {
 
     async function fetchResults() {
         try {
-            let result = await fetch(BACKEND_PATH);
+            let result = await fetch(BACKEND_PATH + textValue);
             let obj = await result.json();
+            return obj;
         } catch(err) {
             console.error(err);
         }
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let obj = await fetchResults();
+        console.log(obj);
     }
 
     function parseToBackend(str) {
@@ -29,10 +36,11 @@ function SearchBar() {
     return (
         <nav className="searchHeader">
             <Link to="/"><img src={logoImage} className="logo"/></Link>
-            <div className="searchDiv">
+            <form className="searchDiv" onSubmit={handleSubmit}>
                 <label htmlFor="searchInput" className="searchIcon"><FontAwesomeIcon icon={faMagnifyingGlass} /></label>
                 <input type="search" placeholder="Search For Recipes" className="searchInput" id="searchInput" onInput={inputChanged} />
-            </div>
+                <button>Submit</button>
+            </form>
         </nav>
     )
 }
