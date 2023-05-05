1bb53d381dbc1603463f52a0f956c3f05c884380
proffitlemon@gmail.com <45953538+Henryn21@users.noreply.github.com>
2023-05-04 17:41:16 -0700

    Router added
    Links for home and recipe pages added in header

--- upcycling-recipe-book/src/components/TheHeader.js
+++ upcycling-recipe-book/src/components/TheHeader.js
@@ -1,11 +1,12 @@
 import logoImage from "../images/UpLogo.PNG";
 import "../stylesheets/TheHeader.css";
+import { Link } from "react-router-dom";
 function TheHeader() {
     return (
         <nav className="myNav">
             <img src={logoImage} className="logo"/>
-            <h1>Home</h1>
-            <h1>Recipes</h1>
+            <h1><Link to="/">Home</Link></h1>
+            <h1><Link to="/SearchPage">Recipes</Link></h1>
             <h1>Sign Up</h1>
             <h1>Log In</h1>
             <h1>Help</h1>
--- upcycling-recipe-book/src/index.js
+++ upcycling-recipe-book/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SearchPage from './components/SearchPage';
+const router = createBrowserRouter([
+  {
+    path: "/", // This is the relative URL path which users can bookmark and navigate to
+    element: <App />, // This is the React Component to load when navigating to the associated path URL
+  },
+  {
+    path: "/SearchPage",
+    element: <SearchPage />,
+  }
+   // More routes...
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
