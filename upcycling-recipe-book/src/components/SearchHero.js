535ed7d3deb73093a683eedbcaa30a3da18489ab
proffitlemon@gmail.com <45953538+Henryn21@users.noreply.github.com>
2023-05-04 18:30:48 -0700

    Started search page components

---
+++ upcycling-recipe-book/src/components/SearchHero.js
@@ -0,0 +1,11 @@
+
+import "../stylesheets/SearchHero.css";
+function SearchHero() {
+    return (
+        <div className="searchHero">
+            
+        </div>
+    )
+}
+
+export default SearchHero;
--- upcycling-recipe-book/src/components/SearchPage.js
+++ upcycling-recipe-book/src/components/SearchPage.js
@@ -1,11 +1,14 @@
 import SearchBar from "./SearchBar";
-// import "../stylesheets/SearchPage.css";
+import SearchHero from "./SearchHero";
+import "../stylesheets/SearchPage.css";
 function SearchPage() {
     return (
         <div>
             <SearchBar/>
             <div className="searchPageBody">
-                <div className="searchPageHero">
+                <div className="searchPageHeroDiv">
+                    <SearchHero></SearchHero>
+                    <SearchHero></SearchHero>
                 </div>
                 <div className="searchCategories">
                     <div className="category"></div>
