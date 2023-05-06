36447747c609fb043d3bb5fbd672e6cc37011149
chaker <chakerbaloch90@gmail.com>
2023-05-05 20:02:55 -0700

    modify server.js file, include new code to search item on the basis of title and supplies, use case insensitive regex

--- upcycling-recipe-book/server/server.js
+++ upcycling-recipe-book/server/server.js
@@ -1,13 +1,11 @@
-
 require('dotenv').config();
 const express = require('express') 
 const cors = require('cors') 
-const port = 3000
+const port = 4000
 const app = express()
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.dbUserName}:${process.env.dbUserPassword}@${process.env.dbClusterName}.${process.env.dbMongoId}.mongodb.net/?retryWrites=true&w=majority`;
-
 
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,8 +17,6 @@ const client = new MongoClient(uri, {
   }
 });
 
-
-
 // for parsing application/json requests
 app.use(express.json())
 // for parsing application/x-www-form-urlencoded requests
@@ -28,44 +24,46 @@ app.use(express.urlencoded({ extended: true }))
 // for allowing different domain origins to make requests to this API
 app.use(cors())
 
-const recepieList = [
-    {
-      name: 'Mason Jar Lanterns',
-      link: 'https://www.makelifelovely.com/diy-mason-jar-lanterns/'
-    },
-    {
-      name: 'Mason Jar Terrarium',
-      link: 'https://www.huffpost.com/entry/diy-mason-jar-terrarium-t_b_8210924'
-    },
-    {
-      name: 'Wine Bottle Lights',
-      link: 'https://diyprojects.com/create-wine-bottle-lights/'
-    }
-  ]
+// middleware function to handle server errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-  //Get method to retrieve a recipe
-  app.get('/recipeList/retrieve/:itemname', async (req, res) => {
-    try {
-      await client.connect()
-      const collection = client.db(process.env.dbName).collection(process.env.dbCollectionName)
-      
-      // Create a text index on the 'name' field
-      //await collection.createIndex({ name: "text" });
-  
-      const result = await collection.find(
-       {title: req.params.itemname  }, 
-        { projection: { _id: 0 } }
-      ).toArray()
-      console.log(result)
-      res.status(200).json(result)
-  
-    } catch (error) {
-      console.log(error)
-      res.sendStatus(500)
-  
-    } finally {
-      setTimeout(() => {client.close()}, 1500)    }
-  });
+//Get method to retrieve a recipe
+app.get('/recipeList/retrieve/:itemname', async (req, res) => {
+  try {
+    const itemName = req.params.itemname;
+    const query = {
+      $or: [
+        { title: { $regex: new RegExp(itemName, 'i') } },
+        {
+          supplies: {
+            $elemMatch: {
+              $regex: new RegExp(itemName, 'i')
+            }
+          }
+        },
+        {
+          supplies: {
+            $type: 'string',
+            $regex: new RegExp(itemName, 'i')
+          }
+        }
+      ]
+    };
+    
+    await client.connect();
+    const collection = client.db(process.env.dbName).collection(process.env.dbCollectionName);
+
+    const results = await collection.find(query, { projection: { _id: 0 } }).limit(10).toArray();
+
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 // Start Express server to listen for API connections
 app.listen(port, () => console.log(`API listening at http://localhost:${port}/`))
